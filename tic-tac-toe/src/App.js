2c18897d0087d2d6d55e30493f1dc10d90b2679a
sunilchauhan2415 <sc053038@gmail.com>
2024-07-30 22:21:45 +0530

    First Commit

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react'
+import TICTACTOE from "./Component/TICTACTOE/TICTACTOE.jsx";
+
+
+
+const App = () => {
+  return (
+    <div>
+      <TICTACTOE />
+    </div>
+  );
+}
+
+export default App
+
+
